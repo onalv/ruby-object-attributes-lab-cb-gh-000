d7fac811166536e9f16bb05075f719025d8c98a4
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-09 15:36:02 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,17 @@
 class Person
   def name
-
+    @name
   end
 
-  def name=
-
+  def name=(new_name)
+    @name = new_name
   end
 
   def job
-
+    @job
   end
 
-  def job=
-
+  def job=(new_job)
+    @job = new_job
   end
 end
